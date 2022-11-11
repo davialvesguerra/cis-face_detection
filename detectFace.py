1bb0502830b284963251ea64931717ce17ffcf68
F2315771 DAVI GUERRA ALVES <davi.guerra@bb.com.br>
2022-11-11 17:23:21 -0300

    colocando a detecção de emoção no código

--- detectFace.py
+++ detectFace.py
@@ -1,6 +1,14 @@
-#Importar o opencsv
+#%%Importar o opencsv
 import cv2
+import tensorflow as tf
+import numpy as np
 
+#%%
+model = tf.keras.models.load_model('model_optimal.h5')
+
+label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+
+#%%
 # importar o modelo treinado
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -12,6 +20,22 @@ vc = cv2.VideoCapture(0)
 if vc.isOpened(): # try to get the first frame
     #esse vc.read() retorna um frame e esse rval que eu não sei o que é
     rval, frame = vc.read()
+
+    frame_resize = cv2.resize(frame, (48, 48))
+    frame_resize = frame_resize[:,:,0]
+    bau = np.expand_dims(frame_resize,axis = 0)
+    print(frame_resize.shape) 
+    frame_resize = bau.reshape(1,48,48,1)
+
+    result = model.predict(frame_resize)
+    result = list(result[0])
+
+
+    label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+    img_index = result.index(max(result))
+    print(label_dict[img_index])
+    
+    
 else:
     rval = False
 
@@ -31,10 +55,25 @@ while rval:
     #faz a predição de onde esteja a imagem
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+    frame_resize = cv2.resize(frame, (48, 48))
+    frame_resize = frame_resize[:,:,0]
+    bau = np.expand_dims(frame_resize,axis = 0)
+
+    frame_resize = bau.reshape(1,48,48,1)
+
+    result = model.predict(frame_resize)
+    result = list(result[0])
+
+
+    label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+    img_index = result.index(max(result))
+    label_final = label_dict[img_index]
+
     #para cada coordenada de onde esteja a face da pessoa
     #ele vai desenhar um retângulo
     for (x, y, w, h) in faces:
       cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+      cv2.putText(frame, label_final, (50, 50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),  2,cv2.LINE_AA)
 
 vc.release()
 cv2.destroyWindow("preview")
